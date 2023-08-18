db1db3a1abcca3573d3f9cb023030760e5ed76f3
Aji Anaz <aji.anaz@gmail.com>
2023-08-18 21:24:53 +0700

    Init Commit Package

---
+++ example/multipurpose_widgetkit_example.dart
@@ -0,0 +1,6 @@
+import 'package:multipurpose_widgetkit/multipurpose_widgetkit.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/multipurpose_widgetkit_test.dart
@@ -0,0 +1,16 @@
+import 'package:multipurpose_widgetkit/multipurpose_widgetkit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
